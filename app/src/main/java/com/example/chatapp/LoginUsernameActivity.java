31cb54053d482741a1fc7ebb74a0ea82901a9fb0
Omprakash1353 <Omprakash13053@gmail.com>
2024-04-05 07:25:32 +0530

    Completed

--- app/src/main/java/com/example/chatapp/LoginUsernameActivity.java
+++ app/src/main/java/com/example/chatapp/LoginUsernameActivity.java
@@ -1,21 +1,31 @@
 package com.example.chatapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.example.chatapp.model.UserModel;
+import com.example.chatapp.utils.AndroidUtils;
 import com.example.chatapp.utils.FirebaseUtil;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentSnapshot;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class LoginUsernameActivity extends AppCompatActivity {
 
@@ -24,6 +34,9 @@ public class LoginUsernameActivity extends AppCompatActivity {
     ProgressBar nextProgress;
     String phoneNumber;
     UserModel userModel;
+    ImageView img;
+    ActivityResultLauncher<Intent> imagePickerLauncher;
+    Uri selectedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +46,55 @@ public class LoginUsernameActivity extends AppCompatActivity {
         username = findViewById(R.id.username);
         next = findViewById(R.id.next);
         nextProgress = findViewById(R.id.nextProgress);
+        img = findViewById(R.id.user_pic);
 
         phoneNumber = getIntent().getExtras().getString("phone");
         getUsername();
 
+        imagePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
+                if (data != null && data.getData() != null) {
+                    selectedImageUri = data.getData();
+                    AndroidUtils.setProfilePic(getApplicationContext(), selectedImageUri, img);
+                }
+            }
+        });
+
+        img.setOnClickListener(v -> {
+            ImagePicker.with(this).cropSquare().compress(512).maxResultSize(512, 512).createIntent(new Function1<Intent, Unit>() {
+                @Override
+                public Unit invoke(Intent intent) {
+                    imagePickerLauncher.launch(intent);
+                    return null;
+                }
+            });
+        });
+
         next.setOnClickListener((v) -> {
             setUsername();
+            updateImg();
+        });
+    }
+
+    void updateImg() {
+        setInProgress(true);
+        if (selectedImageUri != null) {
+            FirebaseUtil.getCurrentProfilePicStorageRef().putFile(selectedImageUri).addOnCompleteListener(task -> {
+                updateToFirestore();
+            });
+
+        }
+    }
+
+    void updateToFirestore() {
+        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(task -> {
+            setInProgress(false);
+            if (task.isSuccessful()) {
+                AndroidUtils.showToast(getApplicationContext(), "Updated successfully");
+            } else {
+                AndroidUtils.showToast(getApplicationContext(), "Failed to update");
+            }
         });
     }
 
